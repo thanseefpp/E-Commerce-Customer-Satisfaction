5439dd06c23be6948abf5ac2c7ebad3f53ca951b
thanseefpp <androtsf@gmail.com>
2023-08-10 22:46:12 +0400

    data cleaning pipeline updated

--- src/CustomerSatisfaction/components/data_cleaning.py
+++ src/CustomerSatisfaction/components/data_cleaning.py
@@ -1,6 +1,7 @@
 from src.CustomerSatisfaction.config.exception import CustomException
 from src.CustomerSatisfaction.config.logger import logging
 from sklearn.model_selection import train_test_split
+from typing_extensions import Annotated
 from abc import ABC, abstractmethod
 from typing import Union,Tuple
 from zenml import step
@@ -28,7 +29,7 @@ class DataPreprocessStrategy(DataStrategy):
         Removes columns which are not required, fills missing values with median average values, and converts the data type to float.
         """
         try:
-            data = data.drop(
+            data.drop(
                 [
                     "order_approved_at",
                     "order_delivered_carrier_date",
@@ -38,7 +39,7 @@ class DataPreprocessStrategy(DataStrategy):
                     "customer_zip_code_prefix",
                     "order_item_id"
                 ],
-                axis=1,inplace=True
+                axis=1,inplace=True,
             )
             data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
             data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
@@ -86,5 +87,24 @@ class DataCleaning:
 
 
 @step
-def clean_df(df:pd.DataFrame)-> None:
-    pass
+def clean_data(data: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "x_train"],
+    Annotated[pd.DataFrame, "x_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"]]:
+    """Data cleaning class which preprocesses the data and divides it into train and test data.
+
+    Args:
+        data: pd.DataFrame
+    """
+    try:
+        preprocess_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(data, preprocess_strategy)
+        preprocessed_data = data_cleaning.handle_data()
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(preprocessed_data, divide_strategy)
+        x_train, x_test, y_train, y_test = data_cleaning.handle_data()
+        return x_train, x_test, y_train, y_test
+    except Exception as e:
+        logging.error("Error occurred from clean_data method from data cleaning module")
+        raise CustomException(e,sys) from e
--- src/CustomerSatisfaction/pipeline/train_pipeline.py
+++ src/CustomerSatisfaction/pipeline/train_pipeline.py
@@ -1,14 +1,15 @@
 from zenml import pipeline
 from src.CustomerSatisfaction.components.data_ingestion import ingest_df
-from src.CustomerSatisfaction.components.data_cleaning import clean_df
+from src.CustomerSatisfaction.components.data_cleaning import clean_data
 from src.CustomerSatisfaction.components.model_train import train_model
 from src.CustomerSatisfaction.components.model_evaluation import evaluate_model
 
 
-@pipeline
+@pipeline(enable_cache=False)
 def train_pipeline(data_path:str):
     df = ingest_df(data_path)
-    clean_df(df)
+    x_train, x_test, y_train, y_test = clean_data(df)
+    print(x_train,y_train)
     train_model(df)
     evaluate_model(df)
     
