16b9354786a72ee55cf489529ae9892f207085d0
thanseefpp <androtsf@gmail.com>
2023-08-10 22:32:08 +0400

    eda started and data clean methods,class splitted

--- src/CustomerSatisfaction/components/data_cleaning.py
+++ src/CustomerSatisfaction/components/data_cleaning.py
@@ -1,8 +1,89 @@
 from src.CustomerSatisfaction.config.exception import CustomException
 from src.CustomerSatisfaction.config.logger import logging
-import sys
+from sklearn.model_selection import train_test_split
+from abc import ABC, abstractmethod
+from typing import Union,Tuple
 from zenml import step
 import pandas as pd
+import numpy as np
+import sys
+
+
+class DataStrategy(ABC):
+    """
+    Abstract Class defining strategy for handling data
+    """
+
+    @abstractmethod
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        pass
+    
+class DataPreprocessStrategy(DataStrategy):
+    """
+    Data preprocessing strategy which preprocesses the data.
+    """
+
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Removes columns which are not required, fills missing values with median average values, and converts the data type to float.
+        """
+        try:
+            data = data.drop(
+                [
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date",
+                    "order_purchase_timestamp",
+                    "customer_zip_code_prefix",
+                    "order_item_id"
+                ],
+                axis=1,inplace=True
+            )
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
+            data["review_comment_message"].fillna("No review", inplace=True)
+            return data.select_dtypes(include=[np.number])
+        except Exception as e:
+            logging.error("Error occurred from handle_data method from DataPreprocessStrategy class")
+            raise CustomException(e,sys) from e
+
+class DataDivideStrategy(DataStrategy):
+    """
+    Data dividing strategy which divides the data into train and test data.
+    """
+
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Divides the data into train and test data.
+        """
+        try:
+            X = data.drop("review_score", axis=1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error("Error occurred from handle_data method from DataDivideStrategy class")
+            raise CustomException(e,sys) from e
+
+class DataCleaning:
+    """
+    Data cleaning class which preprocesses the data and divides it into train and test data.
+    """
+
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy) -> None:
+        """Initializes the DataCleaning class with a specific strategy."""
+        self.df = data
+        self.strategy = strategy
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+        """Handle data based on the provided strategy"""
+        return self.strategy.handle_data(self.df)
+
 
 @step
 def clean_df(df:pd.DataFrame)-> None:
